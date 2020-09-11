9cef61f84b4c19aaa98204597b7e321d58307737
Rodrigo Bastos <rodrigo.alysson@totvs.com.br>
2020-09-11 00:30:43 -0300

    feat(pagination): adding pagination
    Adding pagination in the component to obtain partial image
    
    FT-0007

--- aluraPic/src/app/photos/photo-list/filter-by-description.pipe.ts
+++ aluraPic/src/app/photos/photo-list/filter-by-description.pipe.ts
@@ -4,7 +4,7 @@ import { Photo } from '../photo/photo';
 @Pipe({
     name: 'filterByDescription'
 })
-export class filterByDescription implements PipeTransform {
+export class FilterByDescription implements PipeTransform {
     transform(photos: Photo[], descriptionQuery: string) {
         descriptionQuery = descriptionQuery.trim().toLowerCase();
 
--- aluraPic/src/app/photos/photo-list/photo-list.component.ts
+++ aluraPic/src/app/photos/photo-list/photo-list.component.ts
@@ -4,9 +4,10 @@ import { Subject } from 'rxjs';
 import { debounceTime } from 'rxjs/operators';
 
 import { Photo } from '../photo/photo';
+import { PhotoService } from '../photo/photo.service';
 
 @Component({
-  selector: 'app-photo-list',
+  selector: 'ap-photo-list',
   templateUrl: './photo-list.component.html',
   styleUrls: ['./photo-list.component.css']
 })
@@ -14,10 +15,17 @@ export class PhotoListComponent implements OnInit, OnDestroy {
   photos: Photo[] = [];
   filter: string = '';
   debounce: Subject<string> = new Subject<string>();
+  hasMore: boolean = true;
+  currentPage: number = 1;
+  userName: string = '';
 
-  constructor(private activatedRoute: ActivatedRoute) { }
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private photoService: PhotoService
+  ) { }
   
   ngOnInit(): void {
+    this.userName = this.activatedRoute.snapshot.params.userName;
     this.photos = this.activatedRoute.snapshot.data.photos;
 
     this.debounce
@@ -27,6 +35,15 @@ export class PhotoListComponent implements OnInit, OnDestroy {
 
   ngOnDestroy(): void {
     this.debounce.unsubscribe();
+  }
+
+  load() {
+    this.photoService.listFromUserPaginated(this.userName, ++this.currentPage)
+      .subscribe(photos => {
+        this.photos = this.photos.concat(photos);
+
+        if (!photos.length) this.hasMore = false;
+      });
   }
 
 }
