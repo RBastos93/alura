cea4b69248c08186b3201db0f35a44d65930aecc
Rodrigo <rodrigo.alysson@totvs.com.br>
2020-12-30 08:56:43 -0300

    docs(typescript): readonly properties
    FT-0031

--- typescript/alurabank/app/ts/models/Negotiation.ts
+++ typescript/alurabank/app/ts/models/Negotiation.ts
@@ -1,20 +1,8 @@
 export class Negotiation {
 
-    constructor(private _date: Date, private _amount: number, private _value: number) { }
-
-    get date(): Date {
-        return this._date;
-    }
-
-    get amount(): number {
-        return this._amount;
-    }
-
-    get value(): number {
-        return this._value;
-    }
+    constructor(readonly date: Date, readonly amount: number, readonly value: number) { }
 
     get volume(): number {
-        return this._amount * this._value;
+        return this.amount * this.value;
     }
 }
